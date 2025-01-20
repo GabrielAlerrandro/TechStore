f2ac8bf0d203944434a7ebbf720b280bbf7a3b1f
Gabriel Alerrandro <gabrielalerrandro081@gmail.com>
2025-01-19 23:25:47 -0300

    feat: :sparkles: implement theme management

---
+++ front-end/src/hooks/useTheme.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useEffect, useState } from "react"
+
+interface ThemeContextInterface {
+  theme: string
+  setTheme: (theme: string) => void
+}
+
+const ThemeContext = createContext<ThemeContextInterface | undefined>(undefined)
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const THEME_FROM_LOCAL_STORAGE = localStorage.getItem("theme")
+  const [theme, setTheme] = useState(
+    THEME_FROM_LOCAL_STORAGE === "dark" ? "light" : "dark"
+  )
+
+  useEffect(() => {
+    const root = document.documentElement
+    const removeOldTheme = theme === "dark" ? "light" : "dark"
+    root.classList.remove(removeOldTheme)
+    root.classList.add(theme)
+    localStorage.setItem("theme", theme)
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext)
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider")
+  }
+  return context
+}
